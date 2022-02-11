b0982a2a9288c6fb6deaeeff135f676c08157f45
antoshibaz <29843421+antoshibaz@users.noreply.github.com>
2022-02-11 20:54:17 +0300

    refactor: change yolov5 convertion script gen_wts.py for import-friendly

--- yolov5/gen_wts.py
+++ yolov5/gen_wts.py
@@ -1,9 +1,8 @@
-import sys
 import argparse
 import os
 import struct
+
 import torch
-from utils.torch_utils import select_device
 
 
 def parse_args():
@@ -19,30 +18,44 @@ def parse_args():
         args.output = os.path.join(
             args.output,
             os.path.splitext(os.path.basename(args.weights))[0] + '.wts')
-    return args.weights, args.output
+    return args
 
 
-pt_file, wts_file = parse_args()
+def convert(pt_file, wts_file):
+    # Initialize
+    # device = select_device('cpu')
+    device = torch.device('cpu')
 
-# Initialize
-device = select_device('cpu')
-# Load model
-model = torch.load(pt_file, map_location=device)['model'].float()  # load to FP32
+    # Load model
+    model = torch.load(pt_file, map_location=device)['model'].float()  # load to FP32
 
-# update anchor_grid info
-anchor_grid = model.model[-1].anchors * model.model[-1].stride[...,None,None]
-# model.model[-1].anchor_grid = anchor_grid
-delattr(model.model[-1], 'anchor_grid')  # model.model[-1] is detect layer
-model.model[-1].register_buffer("anchor_grid",anchor_grid) #The parameters are saved in the OrderDict through the "register_buffer" method, and then saved to the weight.
+    # update anchor_grid info
+    anchor_grid = model.model[-1].anchors * model.model[-1].stride[..., None, None]
+    # model.model[-1].anchor_grid = anchor_grid
+    delattr(model.model[-1], 'anchor_grid')  # model.model[-1] is detect layer
+    # The parameters are saved in the OrderDict through the "register_buffer" method, and then saved to the weight.
+    model.model[-1].register_buffer("anchor_grid", anchor_grid)
+    model.to(device).eval()
 
-model.to(device).eval()
+    with open(wts_file, 'w') as f:
+        f.write('{}\n'.format(len(model.state_dict().keys())))
+        for k, v in model.state_dict().items():
+            vr = v.reshape(-1).cpu().numpy()
+            f.write('{} {} '.format(k, len(vr)))
+            for vv in vr:
+                f.write(' ')
+                f.write(struct.pack('>f', float(vv)).hex())
+            f.write('\n')
 
-with open(wts_file, 'w') as f:
-    f.write('{}\n'.format(len(model.state_dict().keys())))
-    for k, v in model.state_dict().items():
-        vr = v.reshape(-1).cpu().numpy()
-        f.write('{} {} '.format(k, len(vr)))
-        for vv in vr:
-            f.write(' ')
-            f.write(struct.pack('>f' ,float(vv)).hex())
-        f.write('\n')
+    print('Pytorch model {} conversion to wts file {} is successfully completed'.format(pt_file, wts_file))
+
+
+def run(args):
+    pt_file = args.weights
+    wts_file = args.output
+    convert(pt_file, wts_file)
+
+
+if __name__ == '__main__':
+    cli_args = parse_args()
+    run(cli_args)
